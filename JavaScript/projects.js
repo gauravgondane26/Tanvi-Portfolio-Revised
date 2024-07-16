8873af0a7db109da1adb21639709fd18d9fab7f5
Gaurav Gondane <gauravgondane1@gmail.com>
2024-07-15 17:49:33 -0700

    ninth commit

--- JavaScript/projects.js
+++ JavaScript/projects.js
@@ -1,132 +1,119 @@
-// let currentIndex = 0;
-// function showSlide(index) {
-//     const slides = document.querySelectorAll('.carousel-item');
-//     const totalSlides = slides.length;
+var cfContainers = document.getElementsByClassName("coverflow");
+var cfLinks = [];
 
-//     if (index >= totalSlides) {
-//         currentIndex = 0;
-//     } else if (index < 0) {
-//         currentIndex = totalSlides - 1;
-//     } else {
-//         currentIndex = index;
-//     }
-
-//     const newTransformValue = -currentIndex * 100;
-//     document.querySelector('.carousel-inner').style.transform = `translateX(${newTransformValue}%)`;
-// }
-
-// function nextSlide() {
-//     showSlide(currentIndex + 1);
-// }
-
-// function prevSlide() {
-//     showSlide(currentIndex - 1);
-// }
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     showSlide(currentIndex);
-// });
-var a = document.getElementsByTagName("a");
-var cfImg = document.getElementsByClassName("coverflow__image")
+for (var i = 0; i < cfContainers.length; i++) {
+    var links = cfContainers[i].getElementsByTagName("a");
+    for (var j = 0; j < links.length; j++) {
+        cfLinks.push(links[j]);
+    }
+}
 
 var scaleI = 0;
-for (scaleI; scaleI < a.length; scaleI++) {
-if (scaleI === 3) {
-    continue;
-} else {
-    a[scaleI].style.cursor = "default";
-    a[scaleI].addEventListener("click", prevDef);
-}
+for (scaleI; scaleI < cfLinks.length; scaleI++) {
+    if (scaleI === 3) {
+        continue;
+    } else {
+        cfLinks[scaleI].style.cursor = "default";
+        cfLinks[scaleI].addEventListener("click", prevDef);
+    }
 }
 
 function prevDef(e) {
-e.preventDefault();
+    e.preventDefault();
 }
 
 function forScale(coverflowPos) {
-for (scaleI = 0; scaleI < a.length; scaleI++) {
-    a[scaleI].style.cursor = "default";
-    a[scaleI].addEventListener("click", prevDef);
-}
-for (scaleI = 0; scaleI < cfImg.length; scaleI++) {
-    if (cfImg[scaleI].getAttribute("data-coverflow-index") == coverflowPos) {
-    cfImg[scaleI].parentElement.style.cursor = "pointer";
-    cfImg[scaleI].parentElement.removeEventListener("click", prevDef);
+    for (scaleI = 0; scaleI < cfLinks.length; scaleI++) {
+        cfLinks[scaleI].style.cursor = "default";
+        cfLinks[scaleI].addEventListener("click", prevDef);
     }
-}
+    for (scaleI = 0; scaleI < cfImg.length; scaleI++) {
+        if (cfImg[scaleI].getAttribute("data-coverflow-index") == coverflowPos) {
+            cfImg[scaleI].parentElement.style.cursor = "pointer";
+            cfImg[scaleI].parentElement.removeEventListener("click", prevDef);
+        }
+    }
 }
 
 function setupCoverflow(coverflowContainer) {
-var coverflowContainers;
+    var coverflowContainers;
 
-if (typeof coverflowContainer !== "undefined") {
-    if (Array.isArray(coverflowContainer)) {
-    coverflowContainers = coverflowContainer;
+    if (typeof coverflowContainer !== "undefined") {
+        if (Array.isArray(coverflowContainer)) {
+            coverflowContainers = coverflowContainer;
+        } else {
+            coverflowContainers = [coverflowContainer];
+        }
     } else {
-    coverflowContainers = [coverflowContainer];
+        coverflowContainers = Array.prototype.slice.apply(document.getElementsByClassName('coverflow'));
     }
-} else {
-    coverflowContainers = Array.prototype.slice.apply(document.getElementsByClassName('coverflow'));
-}
 
-coverflowContainers.forEach(function(containerElement) {
-    var coverflow = {};
-    var prevArrows, nextArrows;
+    coverflowContainers.forEach(function(containerElement) {
+        var coverflow = {};
+        var prevArrows, nextArrows;
 
-    coverflow.container = containerElement;
-    coverflow.images = Array.prototype.slice.apply(containerElement.getElementsByClassName('coverflow__image'));
-    coverflow.position = Math.floor(coverflow.images.length / 2) + 1;
+        coverflow.container = containerElement;
+        coverflow.images = Array.prototype.slice.apply(containerElement.getElementsByClassName('coverflow__image'));
+        coverflow.position = Math.floor(coverflow.images.length / 2) + 1;
 
-    coverflow.images.forEach(function(coverflowImage, i) {
-    coverflowImage.dataset.coverflowIndex = i + 1;
+        coverflow.images.forEach(function(coverflowImage, i) {
+            coverflowImage.dataset.coverflowIndex = i + 1;
+        });
+
+        coverflow.container.dataset.coverflowPosition = coverflow.position;
+
+        prevArrows = Array.prototype.slice.apply(coverflow.container.getElementsByClassName("prev-arrow"));
+        nextArrows = Array.prototype.slice.apply(coverflow.container.getElementsByClassName("next-arrow"));
+
+        function setPrevImage() {
+            coverflow.position = Math.max(1, coverflow.position - 1);
+            coverflow.container.dataset.coverflowPosition = coverflow.position;
+            forScale(coverflow.position);
+        }
+
+        function setNextImage() {
+            coverflow.position = Math.min(coverflow.images.length, coverflow.position + 1);
+            coverflow.container.dataset.coverflowPosition = coverflow.position;
+            forScale(coverflow.position);
+        }
+
+        function jumpToImage(evt) {
+            coverflow.position = Math.min(coverflow.images.length, Math.max(1, evt.target.dataset.coverflowIndex));
+            coverflow.container.dataset.coverflowPosition = coverflow.position;
+            setTimeout(function() {
+                forScale(coverflow.position);
+            }, 1);
+        }
+
+        function onKeyPress(evt) {
+            switch (evt.which) {
+                case 37:
+                    setPrevImage();
+                    break;
+                case 39:
+                    setNextImage();
+                    break;
+            }
+        }
+        prevArrows.forEach(function(prevArrow) {
+            prevArrow.addEventListener('click', setPrevImage);
+        });
+        nextArrows.forEach(function(nextArrow) {
+            nextArrow.addEventListener('click', setNextImage);
+        });
+        coverflow.images.forEach(function(image) {
+            image.addEventListener('click', jumpToImage);
+        });
+        window.addEventListener('keyup', onKeyPress);
     });
-
-    coverflow.container.dataset.coverflowPosition = coverflow.position;
-
-    prevArrows = Array.prototype.slice.apply(coverflow.container.getElementsByClassName("prev-arrow"));
-    nextArrows = Array.prototype.slice.apply(coverflow.container.getElementsByClassName("next-arrow"));
-
-    function setPrevImage() {
-    coverflow.position = Math.max(1, coverflow.position - 1);
-    coverflow.container.dataset.coverflowPosition = coverflow.position;
-    forScale(coverflow.position);
-    }
-
-    function setNextImage() {
-    coverflow.position = Math.min(coverflow.images.length, coverflow.position + 1);
-    coverflow.container.dataset.coverflowPosition = coverflow.position;
-    forScale(coverflow.position);
-    }
-
-    function jumpToImage(evt) {
-    coverflow.position = Math.min(coverflow.images.length, Math.max(1, evt.target.dataset.coverflowIndex));
-    coverflow.container.dataset.coverflowPosition = coverflow.position;
-    setTimeout(function() {
-        forScale(coverflow.position);
-    }, 1);
-    }
-
-    function onKeyPress(evt) {
-    switch (evt.which) {
-        case 37:
-        setPrevImage();
-        break;
-        case 39:
-        setNextImage();
-        break;
-    }
-    }
-    prevArrows.forEach(function(prevArrow) {
-    prevArrow.addEventListener('click', setPrevImage);
-    });
-    nextArrows.forEach(function(nextArrow) {
-    nextArrow.addEventListener('click', setNextImage);
-    });
-    coverflow.images.forEach(function(image) {
-    image.addEventListener('click', jumpToImage);
-    });
-    window.addEventListener('keyup', onKeyPress);
-});
 }
 
 setupCoverflow();
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const link = document.querySelector('.nav-links'); // Select the anchor tag
+    link.addEventListener('click', function(e) {
+        e.preventDefault(); // Prevent the default link action
+        // Additional actions can be added here
+    });
+});
